0c4150f58b1adc3ad85b0cd8cf75feee79b4ba9f
basjqnatsks <mjhfraud@gmail.com>
2019-04-25 18:05:57 -0700

    init

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
-#from .install import *
+from .install import *
 from .freeze import freeze
 from .sleepD import SleepD
 from .read import read
-from .ThreadIT import ThreadIT
 from .AutoBrow import AutoBrow
 from .updater import updater
 from.chrome import chrome
