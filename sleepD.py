b7a0cea7e7bd79ce7a7c0736458f8a3f264acb91
basjqnatsks <mjhfraud@gmail.com>
2019-05-07 19:32:54 -0700

    more accurate sleepd function

--- sleepD.py
+++ sleepD.py
@@ -1,38 +1,18 @@
 from time import sleep
 import os
+import asyncio
 class SleepD:
-    def __init__(self, time=None, method='title', clear=None):
-        self.clear = clear
-        #Title Only is only for >1 in INT
-        #log is Tenth of decimal 
-        if time:
-            if time < 0:
-                raise IOError("Time Less Than 0 in SleepD()")
-            self.time = time
-            if method == 'title':        
-                self.sleeptitle()
-            elif method == 'log':
-                self.sleeplog()
-        else:
-            raise IOError("Try Putting Number in SleepD()") 
-    def sleeptitle(self):
-        self.time = int(self.time)
-        while self.time != -1:
-            if self.time == 0:
-                os.system('title Time Left: ' + str(int(self.time)))
-                self.time -= 1
-                continue
-            os.system('title Time Left: ' + str(int(self.time)))
+    def __init__(self, time: int = None):
+        self.loop = asyncio.get_event_loop()
+        self.time = int(time)       
+        self.loop.run_until_complete(self.sleeptitle())
+
+    async def oscorutine(self):
+        os.system('title Time Left: ' + str(int(self.time)))
+        
+    async def sleeptitle(self):
+        while self.time > 0:
+            await self.oscorutine()
             sleep(1)
             self.time -= 1
-    def sleeplog(self):
-        while self.time != 0:
-            if self.time < 0:
-                self.time = 0
-                if self.clear != None:
-                    os.system('cls')
-                print('Time Left: ' + str(self.time))
-                continue
-            print('Time Left: ' + str(self.time))
-            sleep(0.0166666666666666667)
-            self.time -= 0.0166666666666666667
+        await self.oscorutine()
