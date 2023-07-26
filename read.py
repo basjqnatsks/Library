750138ec00badee29b33f19ce91a57ddf4a4d387
basjqnatsks <mjhfraud@gmail.com>
2023-07-26 11:39:11 -0700

    Added Removal Functionality

--- read.py
+++ read.py
@@ -1,12 +1,17 @@
 class read(object):
-    def __new__(cls, filename: str, delim: str = None, Encoding : str ="ISO-8859-1") -> open:
+    def __new__(cls, filename: str, delim: str = None, Encoding : str ="ISO-8859-1", Remove: list = ['\r', '\t']) -> open:
         cls.Encoding = Encoding
+        cls.Remove = Remove
         if delim != None:
             return cls.fileread(cls, filename).split(delim)
         return cls.fileread(cls, filename)
     def fileread(self, filename: str) -> open:
         with open(filename, "rb") as f:
-            var = f.read().decode(self.Encoding).replace("\r", "").replace("\t", "")
+            if self.Remove:
+                for x in self.Remove:
+                    var = f.read().decode(self.Encoding).replace(x, "")
+            else:
+                var = f.read().decode(self.Encoding)
         return var
 class FileLength(object):
     def __new__(cls, FileString=None):
